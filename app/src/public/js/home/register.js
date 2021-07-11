0fa99314f53e737d5a681ad0e9eebb703e0f7c7a
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2021-07-11 21:10:42 +0900

    회원가입 라우팅 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -9,11 +9,14 @@ const id = document.querySelector('#id'),
 registerBtn.addEventListener('click', register);
 
 function register() {
+  if (!id.value) return alert('아이디를 입력하세요');
+  if (psword.value !== confirmPsword.value)
+    return alert('비밀번호가 일치하지 않습니다.');
+
   const req = {
     id: id.value,
     name: name.value,
     psword: psword.value,
-    confirmPsword: confirmPsword.value,
   };
 
   fetch('/register', {
@@ -27,7 +30,7 @@ function register() {
     .then((res) => res.json())
     .then((res) => {
       if (res.success) {
-        location.href = '/';
+        location.href = '/login';
       } else {
         alert(res.msg);
       }
