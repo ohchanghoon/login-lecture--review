d31c04c4b1447c5b62f083827920152c3d5a7142
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2021-07-20 11:32:13 +0900

    최종 최적화

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,17 +1,21 @@
 'use strict';
 
+const logger = require('../../config/logger');
 const User = require('../../models/User');
 
 const output = {
   home: (req, res) => {
+    logger.info(`GET / 200 " 홈 화면으로 이동`);
     res.render('home/index');
   },
 
   login: (req, res) => {
+    logger.info(`GET /login 200 " 로그인 화면으로 이동`);
     res.render('home/login');
   },
 
   register: (req, res) => {
+    logger.info(`GET /register 200 " 회원가입 화면으로 이동`);
     res.render('home/register');
   },
 };
@@ -20,17 +24,47 @@ const process = {
   login: async (req, res) => {
     const user = new User(req.body);
     const response = await user.login();
-    return res.json(response);
+
+    const url = {
+      method: 'POST',
+      path: '/login',
+      status: response.err ? 400 : 200,
+    };
+    log(response, url);
+    return res.status(url.status).json(response);
   },
 
   register: async (req, res) => {
     const user = new User(req.body);
     const response = await user.register();
-    return res.json(response);
+
+    const url = {
+      method: 'POST',
+      path: '/register',
+      status: response.err ? 409 : 201,
+      w,
+    };
+    log(response, url);
+
+    return res.status(url.status).json(response);
   },
 };
 
 module.exports = {
   output,
   process,
+};
+
+const log = (response, url) => {
+  if (response.err) {
+    logger.error(
+      `${url.method} ${url.path} ${url.status} Response : ${response.success} ${response.err}"`
+    );
+  } else {
+    logger.info(
+      `${url.method} ${url.path} ${url.status} Response : ${
+        response.success
+      }, ${response.msg || ''}`
+    );
+  }
 };
